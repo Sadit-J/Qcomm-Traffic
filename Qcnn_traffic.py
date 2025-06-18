dd233137822ada98f02e721994da6ab05054d9d3
Sadit Joarder <155415619+Sadit-J@users.noreply.github.com>
2025-06-17 22:50:14 -0400

    Added main file and test circuit
    Created a QCNN traffic model for a distributed quantum network. The main files processes given parameters and outputs a circuit file for Qcomm, a distributed quantum network simulator.

---
+++ Qcnn_traffic.py
@@ -0,0 +1,221 @@
+from bitlist import bitlist
+import random
+import math
+import itertools
+
+from matplotlib.style.core import available
+from qiskit.transpiler.passes.analysis import num_qubits
+
+
+class Network():
+    def __init__(self, nodes, num_qubits):
+        self.qubit_per_node = num_qubits
+        self.nodes_list = nodes
+
+    def available_nodes(self):
+        free_nodes = []
+
+        for node in self.nodes_list:
+            if node.current_occupation():
+                free_nodes.append(node)
+
+        return free_nodes
+
+    def free_all_nodes(self):
+        for node in self.nodes_list:
+            node.reset_occupation()
+
+    def get_node(self, node_address):
+        for node in self.nodes_list:
+            if node_address == node.get_address():
+                return node
+
+        return None
+
+    def get_num_qubits(self):
+        return self.qubit_per_node
+
+    def get_num_nodes(self):
+        return len(self.nodes_list)
+
+class Node():
+    def __init__(self, address, num_qubits, length):
+        self.address = bitlist(address, length)
+        self.num_qubits = num_qubits
+        self.qubit_list = []
+        for i in range(num_qubits):
+            self.qubit_list.append("0")
+
+    def get_available_qubits(self):
+        available_qubits = []
+        for qubit_availability in self.qubit_list:
+            if qubit_availability == "0":
+                available_qubits.append(qubit_availability)
+
+        return available_qubits
+
+    def occupy_qubits(self):
+
+        available_qubit = -1
+        unoccupied_qubit = False
+
+        while not unoccupied_qubit:
+            available_qubit = random.randrange(len(self.qubit_list))
+
+            if self.qubit_list[available_qubit] == "0":
+                self.qubit_list[available_qubit] = "1"
+                unoccupied_qubit = True
+
+        return available_qubit
+
+
+    def get_address(self):
+        return self.address
+
+    def reset_occupation(self):
+        i = 0
+        while i < self.num_qubits:
+            if "1" in self.qubit_list:
+                occupied_qubit = self.qubit_list.index("1")
+                self.qubit_list[occupied_qubit] = "0"
+            else:
+                break
+
+            i += 1
+
+    def current_occupation(self):
+        if "0" not in self.qubit_list:
+            return False
+
+        return True
+
+    def discard_qubits(self, discarded_qubits, num_nodes):
+
+        self.qubit_list[(discarded_qubits - int(self.address) ) // num_nodes] = "Z"
+
+
+def write_circuit(circuit, f):
+
+    for src in circuit:
+
+        if len(src) == 1:
+            f.write(f"({src[0]}) ")
+            print(f"Node {int(src[0])}")
+        else:
+
+            print(f"Node {int(src[0])} → Node {int(src[1])}")
+            # print(f"{i}. Qubit {int(src[0].get_address()) * num_qubits + src[0].occupy_qubits()} → Qubit {int(src[1].get_address()) * num_qubits + src[0].occupy_qubits()}")
+            f.write(f"({src[0]} {src[1]}) ")
+
+    return
+
+def conv_param_circuit(qubit_one, qubit_two, circuit_file):
+    return [(qubit_two,), (qubit_two, qubit_one), (qubit_one, qubit_two), (qubit_two,), (qubit_two, qubit_one), (qubit_one,)]
+
+def convolution_operations(occupied_qubits, node_address, circuit_file):
+    combinations = list(itertools.combinations(occupied_qubits, 2))
+    #print("Qubit Combinations")
+    #print(combinations)
+    #circuit_file.write("Conv Layer: ")
+    for i in combinations:
+        conv_circuit = conv_param_circuit(i[0], i[1], circuit_file)
+        write_circuit(conv_circuit, circuit_file)
+
+    circuit_file.write("\n")
+
+    return
+
+def pool_param_circuit(qubit_one, qubit_two, circuit_file ):
+    return [(qubit_two,), (qubit_two, qubit_one), (qubit_one,), (qubit_two,), (qubit_one, qubit_two), (qubit_two,)]
+
+def pooling_operation(occupied_qubits, network, circuit_file):
+
+    random.shuffle(occupied_qubits)
+
+    pairs = []
+    for i in range(0, len(occupied_qubits) - 1, 2):
+        pairs.append((occupied_qubits[i], occupied_qubits[i + 1]))
+
+    #circuit_file.write("Pool Layer: ")
+    for pair in pairs:
+        pool_circuit = pool_param_circuit(pair[0], pair[1], circuit_file)
+        current_node = pair[0] %  16
+        network.get_node(current_node).discard_qubits(pair[0], network.get_num_nodes())
+        occupied_qubits.remove(pair[0])
+
+        write_circuit(pool_circuit, circuit_file)
+
+    circuit_file.write("\n")
+    return
+
+def select_nodes(window_size, network, file):
+    available_nodes = network.available_nodes()
+    leftover_neurons = window_size * window_size
+    workload = []
+
+    if leftover_neurons < network.get_num_qubits() * len(available_nodes):
+        current_node = random.choice(available_nodes)
+        available_nodes.remove(current_node)
+
+        for i in range(leftover_neurons):
+            if not current_node.current_occupation():
+                current_node = random.choice(available_nodes)
+                available_nodes.remove(current_node)
+
+            workload.append(int(current_node.get_address()) + network.get_num_nodes() * current_node.occupy_qubits())
+
+    else:
+        print("Not enough cores for largest slice")
+
+
+    return workload
+
+def cnn_traffic(num_qubits, network, circuit_file):
+    maximum_grouping = num_qubits
+    kernel_size = 2
+    input_size = 5
+
+    workload_list = []
+    layer_len = []
+    layer_pos = 0
+
+    stride_conv = 1
+    stride_pool = 2
+    padding = 0
+
+    i = 0
+
+    window_size = math.floor(((input_size - kernel_size + 2 * padding) / stride_conv) + 1)
+    workload_list = select_nodes(window_size, network, circuit_file)
+    print(workload_list)
+    while len(workload_list) > 1:
+
+        if (i % 2) == 0:
+            #conv layer
+            print("Conv layer")
+            convolution_operations(workload_list, network, circuit_file)
+        else:
+            #pool layer
+            print("Pool layer")
+            pooling_operation(workload_list, network, circuit_file)
+
+        print(workload_list)
+        i += 1
+
+
+
+def main():
+    f = open("QCNN_traffic.txt", "w")
+    num_qubits = 6
+    node_list = []
+    num_nodes = 16
+
+    for i in range(num_nodes):
+        node_list.append(Node(i, num_qubits, int(math.sqrt(num_nodes))))
+
+    current_Network = Network(node_list, num_qubits)
+
+
+    cnn_traffic(num_qubits, current_Network, f)
+
+main()
