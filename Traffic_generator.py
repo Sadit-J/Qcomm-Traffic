9f7b654c667425f08bc31888873fdf413126cdb3
Sadit Joarder <Sadit.Joarder@torontomu.ca>
2025-06-23 10:52:15 -0400

    Added comments to explain the classes

--- Traffic_generator.py
+++ Traffic_generator.py
@@ -2,10 +2,11 @@ from bitlist import bitlist
 import random
 import math
 
+# Class defines the mesh network containing the nodes
 class Network():
     def __init__(self, nodes, node_qubits, total_qubits):
         self.qubit_per_node = node_qubits
-        self.total_qubits = total_qubits
+        self.total_qubits = total_qubits  
         self.nodes_list = nodes
 
     def available_nodes(self):
@@ -21,13 +22,13 @@ class Network():
         for node in self.nodes_list:
             node.reset_occupation()
 
+    #Returns all nodes in the network regardless of occupation
     def get_node(self, node_address):
         for node in self.nodes_list:
             if node_address == node.get_address():
                 return node
 
         return None
-
 
     def get_per_qubits(self):
         return self.qubit_per_node
@@ -43,10 +44,15 @@ class Node():
         self.address = bitlist(address, length)
         self.num_qubits = num_qubits
         self.qubit_list = []
+
+        #Assigns the qubit "0" to define the idle qubits
         for i in range(num_qubits):
             self.qubit_list.append("0")
 
     def get_available_qubits(self):
+
+        # Returns a list of the all the available qubits within
+        # the node
         available_qubits = []
         for qubit_availability in self.qubit_list:
             if qubit_availability == "0":
@@ -56,9 +62,12 @@ class Node():
 
     def occupy_qubits(self):
 
+        # If there are no available qubits returns a -1
         available_qubit = -1
         unoccupied_qubit = False
 
+        # When multiple idle qubits exists, returns the qubit with the lowest number of
+        # utilisation and set its the element to "1" to indicate it's being utilised
         if "0" in self.qubit_list:
             available_qubit = self.qubit_list.index("0")
             self.qubit_list[available_qubit] = "1"
@@ -69,7 +78,9 @@ class Node():
 
     def get_address(self):
         return self.address
+    
 
+    # Resets all qubits to become idle
     def reset_occupation(self):
         i = 0
         while i < self.num_qubits:
@@ -81,12 +92,14 @@ class Node():
 
             i += 1
 
+    # Returns true if there are qubits available, otherwise false
     def current_occupation(self):
         if "0" not in self.qubit_list:
             return False
 
         return True
 
+    # Discards a qubit from use, will not be detected by other functions
     def discard_qubits(self, discarded_qubits, num_nodes):
 
         self.qubit_list[(discarded_qubits - int(self.address) ) // num_nodes] = "Z"
