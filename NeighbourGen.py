3d14fe30d35e1d4f0b33233d79273eae3555a62d
Justin Dang <justinthdang@gmail.com>
2025-08-05 10:06:19 -0400

    fix neighbour and bit reversal duplicate gate generation

--- NeighbourGen.py
+++ NeighbourGen.py
@@ -160,7 +160,7 @@ def neighbourGen(a, b, c, d, q, r, s, t, u):
             # new line if qubit is repeating or the random number generated is greater than 0.5
             if repeating or (random.random() > 0.5 and gate_index != 0):
                 index = string.rfind("G")
-                string = string[:index - 1] + f"\n" + string[index:]
+                string = string[:index - 1] + f"\n"
 
                 # reset qubit tracker and current slice
                 current_slice = []
